dee9affb6f8cdc550e83033568d7f550a7add587
Adrian Jonsson Sjödin <adrian.jonsson.sjodin@omegapoint.se>
2025-04-23 16:34:45 +0200

    Done up to lesson 36
    Created different kinds of injected controllers, none using Spring

---
+++ src/main/java/guru/springframework/spring6di/controller/ConstructorInjectedController.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring6di.controller;
+
+import guru.springframework.spring6di.services.GreetingService;
+
+/**
+ * Better than property and setter. Still not actually using the Spring framework
+ */
+public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/guru/springframework/spring6di/controller/SetterInjectedController.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring6di.controller;
+
+import guru.springframework.spring6di.services.GreetingService;
+
+/**
+ * Not as bad as property injected
+ */
+public class SetterInjectedController {
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
